4a5626f10b35ba5f40afb1bfcbd6809ba4d98e5f
ramshidtegain95 <ramshid@tegain.com>
2022-11-15 13:54:11 +0530

    2nd

--- api/employee/urls.py
+++ api/employee/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^employee/$', views.employee, name='employee'),
+    re_path(r'', views.employee, name='employee'),
     re_path(r'^edit-employee/$', views.edit_employee, name='edit_employee'),
 
 ]
